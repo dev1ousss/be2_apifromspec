e01b8215cbccd50b48b023a201297b6640f22a9e
dev1ousss <miha47419@gmail.com>
2026-08-13 15:14:01 +0300

    Done

--- src/datasource/repositories/image_repository.py
+++ src/datasource/repositories/image_repository.py
@@ -24,10 +24,31 @@ class ImageRepository:
         self.db.refresh(image)
         return image
 
-    def update(self): ...
+    def update(self, image_id: UUID, content: bytes) -> Image | None:
+        image = self.get_by_id(image_id)
+        if image is None:
+            return None
+        image.image = content
+        self.db.commit()
+        self.db.refresh(image)
+        return image
 
-    def delete(self): ...
+    def delete(self, image_id: UUID) -> bool:
+        image = self.get_by_id(image_id)
+        product = self.db.query(Product).where(Product.image_id == image_id).first()
+        if product is not None:
+            product.image_id = None
+        self.db.delete(image)
+        self.db.commit()
+        return True
 
-    def get_by_id(self): ...
+    def get_by_id(self, image_id: UUID) -> Image | None:
+        return self.db.query(Image).where(Image.id == image_id).first()
 
-    def get_by_product_id(self): ...
+    def get_by_product_id(self, product_id: UUID) -> Image | None:
+        return (
+            self.db.query(Image)
+            .join(Product, Product.image_id == Image.id)
+            .filter(Product.id == product_id)
+            .first()
+        )
--- src/web/routers/images.py
+++ src/web/routers/images.py
@@ -2,7 +2,15 @@ from uuid import UUID
 
 from datasource.database import get_db
 from datasource.repositories.image_repository import ImageRepository
-from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, status
+from fastapi import (
+    APIRouter,
+    Depends,
+    File,
+    HTTPException,
+    Response,
+    UploadFile,
+    status,
+)
 from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/images", tags=["images"])
@@ -29,3 +37,60 @@ def upload_image(
     except ValueError as e:
         raise HTTPException(status.HTTP_400_BAD_REQUEST, str(e))
     return {"image_id": image.id}
+
+
+@router.put(
+    "/{image_id}",
+    summary="Change the image",
+    description="Changes the image by its' id",
+)
+def update_image(
+    image_id: UUID,
+    file: UploadFile = File(..., description="New image file"),
+    repo: ImageRepository = Depends(get_images_repository),
+):
+    content = file.file.read()
+    image = repo.update(image_id, content)
+    if image is None:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Image not found")
+    return {"image_id": image.id}
+
+
+@router.get("/{image_id}", summary="Get image by its' id")
+def get_image(image_id: UUID, repo: ImageRepository = Depends(get_images_repository)):
+    image = repo.get_by_id(image_id)
+    if image is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "Image not found")
+    return Response(
+        content=image.image,
+        media_type="application/octet-stream",
+        headers={"Content-Disposition": f"attachment; filename=image_{image.id}.bin"},
+    )
+
+
+@router.delete(
+    "/{image_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    summary="Delete image by its' id",
+)
+def delete_image(
+    image_id: UUID, repo: ImageRepository = Depends(get_images_repository)
+):
+    if not repo.delete(image_id):
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "Image not found")
+    return None
+
+
+@router.get("/product/{product_id}", summary="Get product image by product id")
+def get_image_by_product_id(
+    product_id: UUID, repo: ImageRepository = Depends(get_images_repository)
+):
+    image = repo.get_by_product_id(product_id)
+
+    if image is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "Image not found")
+    return Response(
+        content=image.image,
+        media_type="application/octet-stream",
+        headers={"Content-Disposition": f"attachment; filename=image_{image.id}.bin"},
+    )
